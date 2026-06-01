e145a1927b16435e8bdc2abf540c22143ec134a8
josephincyber <josephinekingombe11@gmail.com>
2026-06-01 21:28:42 +0100

    Fix: Ajoutee encore du prefixe api sur l'url du tunnel

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ import { Transfert, Beneficiaire, Banque } from './models/transfert.model';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'https://ugly-cobra-86.loca.lt/api';
+  private apiUrl = 'https://silver-experts-live.loca.lt';
   //private apiUrl = 'http://localhost:3001/api';
 
   //1. On cree l'entête requise pour contourner la page de localtunnel
